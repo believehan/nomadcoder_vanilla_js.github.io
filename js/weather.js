6b39add8822b3fe81ea95691bbd7cb57a21d1a54
believehan <90814456+believehan@users.noreply.github.com>
2023-12-11 00:36:37 +0900

    Create weather.js

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "eec5ff4c24af98e1bc855d7c4f83dbc6";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+    });
+}
+function onGeoError(){
+    alert("Can't find wou. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
